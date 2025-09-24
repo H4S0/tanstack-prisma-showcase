3b62c80ef649a0f509bc093d27008fee47721d88
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2025-09-24 15:03:44 +0200

    Update route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,8 +1,8 @@
-// app/api/users/route.ts
 import { prismaApiHandler } from '@/app/lib/server-prisma-handler';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
+//api route query showcase
   const users = await prismaApiHandler({
     model: 'user',
     operation: 'findMany',
