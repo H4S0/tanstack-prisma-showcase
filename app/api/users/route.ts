13882f0f5722bd9466fba1a225e3e986d3997f80
H4S0 <hasanalic10@gmail.com>
2025-09-25 11:33:52 +0200

    add kinde auth

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,12 +1,12 @@
-// app/api/users/route.ts
 import { prismaApiHandler } from '@/app/lib/server-prisma-handler';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
+  //api route query showcase
   const users = await prismaApiHandler({
     model: 'user',
     operation: 'findMany',
-    args: { where: { email: 'email@example.test' } },
+    args: { where: { email: 'hasanalic10@gmail.com' } },
   });
 
   return NextResponse.json(users);
